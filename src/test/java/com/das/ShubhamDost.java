891db4b5376085356be26cf952ece80b3aa362c3
anmoldas225 <anmol.cseiimt@gmail.com>
2026-03-28 15:19:10 +0530

    check

--- src/test/java/com/das/ShubhamDost.java
+++ src/test/java/com/das/ShubhamDost.java
@@ -1,5 +1,12 @@
 package com.das;
 
 public class ShubhamDost {
-
+	
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Shubham Mera Acha Dost Hai or bhi acha ban skta hai");
+		
+	}
+	
 }
