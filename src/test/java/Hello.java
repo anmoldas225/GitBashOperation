20ceafdc5b60e4e1a99a8788f750e1921caaf766
anmoldas225 <anmol.cseiimt@gmail.com>
2026-03-28 12:49:37 +0530

    Coomitt

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,19 +1,13 @@
 
 public class Hello {
-	
-	
+
 	public static void main(String[] args) {
-		
+
+		System.out.print("Ruko");
+
 		System.out.println("Hello");
 		System.out.print("Sona");
-		
+
 	}
-
-
-public static void main(String[] args){
-
-System.out.print("Ruko");
-  
-}
 
 }
